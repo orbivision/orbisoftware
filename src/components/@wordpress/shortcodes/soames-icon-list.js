833f0838a1dd0c203c4360fc06dd522490944664
John Eric Fritz <fritz@orbisoftware.com>
2023-01-16 16:33:35 -0600

    Added support for links to icon list shortcode

--- src/components/@wordpress/shortcodes/soames-icon-list.js
+++ src/components/@wordpress/shortcodes/soames-icon-list.js
@@ -3,6 +3,9 @@ import React from "react";
 const SoamesIconList = ({ attributes }) => {
 
   const icons = [];
+  if (!attributes.links) {
+    attributes.links = [];
+  }
   if (!attributes.labels) {
     attributes.labels = [];
   }
@@ -11,13 +14,14 @@ const SoamesIconList = ({ attributes }) => {
   }
   for (const i in attributes.images) {
     const imageUrl = attributes.images[i].replace(/['""]+/g, '"');
-    const menuItem = {
+    const icon = {
       id: 'icon_' + i,
       imageUrl: imageUrl,
       label: attributes.labels[i] ? attributes.labels[i] : null,
+      link: attributes.links[i] ? attributes.links[i] : null,
       css: attributes.css[i] ? attributes.css[i] : null,
     }
-    icons.push(menuItem);
+    icons.push(icon);
   }
 
   return(
@@ -25,7 +29,11 @@ const SoamesIconList = ({ attributes }) => {
       <div className="container">
       <div className="d-flex flex-wrap align-items-center justify-content-center">
         {icons.map(icon => (   
-          <a key={icon.id} className={`d-flex flex-column align-items-center text-decoration-none p-2 ` + icon.css} href="https://www.typescriptlang.org/" target="_blank" rel="noreferrer">
+          <a key={icon.id}
+            className={`d-flex flex-column align-items-center text-decoration-none p-2 ` + icon.css}
+            href={icon.link}
+            target="_blank"
+            rel="noreferrer">
             <img className="d-block mb-2" src={icon.imageUrl} alt="" height="116" loading="lazy" />
             <span className="text-muted">{icon.label}</span>
           </a>
